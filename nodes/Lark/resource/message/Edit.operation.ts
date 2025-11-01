dc7cb03aff77f958ba25cd61eb2bc5627cc6a11b
Thomas Zhang <zhgqthomas@outlook.com>
2025-11-01 21:17:51 +0800

    fix: edit message type is wrong

--- nodes/Lark/resource/message/Edit.operation.ts
+++ nodes/Lark/resource/message/Edit.operation.ts
@@ -12,7 +12,17 @@ export default {
 	order: 198,
 	options: [
 		DESCRIPTIONS.MESSAGE_ID,
-		DESCRIPTIONS.MESSAGE_TYPE,
+		{
+			displayName: 'Message Type(消息类型)',
+			name: 'edit_msg_type',
+			type: 'options',
+			options: [
+				{ name: 'Rich Text(富文本)', value: 'post' },
+				{ name: 'Text(文本)', value: 'text' },
+			],
+			required: true,
+			default: 'post',
+		},
 		DESCRIPTIONS.MESSAGE_CONTENT,
 		{
 			displayName: `<a target="_blank" href="https://open.feishu.cn/document/server-docs/im-v1/message/update">${WORDING.OpenDocument}</a>`,
@@ -23,7 +33,7 @@ export default {
 	],
 	async call(this: IExecuteFunctions, index: number): Promise<IDataObject> {
 		const message_id = this.getNodeParameter('message_id', index) as string;
-		const msg_type = this.getNodeParameter('msg_type', index) as string;
+		const msg_type = this.getNodeParameter('edit_msg_type', index) as string;
 		const content = NodeUtils.getObjectData(this, index, 'content');
 
 		const { data } = await RequestUtils.request.call(this, {
