4741927811fcd4459906ba9e7414ce8dac9dc4cf
Thomas Zhang <zhgqthomas@outlook.com>
2025-11-10 21:03:20 +0800

    fix: remove mcp since it still not stable

--- nodes/Lark/resource/Mcp.resource.ts
+++
@@ -1,8 +0,0 @@
-import { ResourceType } from '../../help/type/enums';
-import { ResourceOptions } from '../../help/type/IResource';
-
-export default {
-	name: 'MCP',
-	value: ResourceType.Mcp,
-	order: 110,
-} as ResourceOptions;
